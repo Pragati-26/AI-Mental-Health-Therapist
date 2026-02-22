46fa58a30da84a44afb76a188d146275f2bb032e
Pragati-26 <pragatikesharwani9@gmail.com>
2026-02-22 15:50:05 +0530

    changes done in backend main.py

--- backend/main.py
+++ backend/main.py
@@ -30,5 +30,3 @@ async def ask(query: Query):
 # ---------------------------
 # Run Server
 # ---------------------------
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
